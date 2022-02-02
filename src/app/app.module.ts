3b9254e98df1802eed09d3f2c1c73cde4866ec46
Karim <mail@ackarim.com>
2022-02-02 16:43:55 +0100

    Componentizing containers 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BtnComponent } from './btn/btn.component';
 import { IntroComponent } from './intro/intro.component';
+import { MobileComponent } from './mobile/mobile.component';
+import { FooterComponent } from './footer/footer.component';
+import { PremiumComponent } from './premium/premium.component';
+import { SocialComponent } from './social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BtnComponent,
-    IntroComponent
+    IntroComponent,
+    MobileComponent,
+    FooterComponent,
+    PremiumComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
